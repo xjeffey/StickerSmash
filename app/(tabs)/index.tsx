17fad0adfd9843a1301089b5b81d1e312c60a244
Jeff Tran <tranjeff@live.com>
2025-03-30 22:27:35 -0400

    working on chapter 2/3 of the tutorial

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
